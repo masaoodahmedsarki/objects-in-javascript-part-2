7f2a465765d83de8604abc05eb9ec759a933442a
Masood Ahmed56 <115347725+masaoodahmedsarki@users.noreply.github.com>
2024-03-12 01:24:19 +0500

    part2 objects in javascript

---
+++ demo17.js
@@ -0,0 +1,53 @@
+// const tinderUser
+const tinderUser ={}
+
+tinderUser.id = "123abc" 
+tinderUser.name = "Masood" 
+tinderUser.isloggedIn = false
+    
+//console.log(tinderUser);
+
+regularUser = {
+    email: "masaood@gmail.com",
+    fullName:{
+        userfullName:{
+            firstname:"Masood",
+            lastname:"Ahmed"
+        }
+    }
+
+}
+
+//console.log(regularUser.fullName.userfullName.firstname)
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"a", 4:"b"}
+const obj4 = {5:"a", 5:"b"}
+//const obj3 ={obj1,obj2}
+//const obj3 = Object.assign({},obj1,obj2,obj4)
+
+const obj3 = {...obj1,...obj2}
+//console.log(obj3);
+
+
+const Users =[
+    {
+            id: 1,
+            email: "masoo@gmail.com"
+
+},
+    {
+            id: 1,
+            email: "masoo@gmail.com"
+
+},
+]
+
+Users[1].email
+console.log(tinderUser)
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isloggedIn'))
